1218eced0e057e096b07c23d65d06d357baed5bf
Dean Shelton <deanshelton913@gmail.com>
2014-06-08 21:40:00 -0700

    added quotes

--- vietnamnom.rb
+++ vietnamnom.rb
@@ -1,7 +1,19 @@
 require 'sinatra'
 
 get '/' do
-  haml :index
+  catch_phrase = [
+  	"Why for - how some it be... That da noodle be hard when dey be no bone in it?",
+  	"100 years Vietnamnom. 100 years, Morty. www.vietnamnom.com. 100 years vietnamnom.comcom",
+  	"Everybody's workin for the noodle.",
+  	"Oh you into noodles? Well I'm a mushroom cloud layin motherfucker, motherfucker.",
+  	"Sir mix a lot would like udon, and you can not lie.",
+  	"The longest noodle ever created was over a mile long. <a href='http://metro.co.uk/2011/03/15/its-the-worlds-longest-noodle-644450/'>read about it</a>",
+  	"Good pho broth is made from beef bone marrow. This is fact. Google it.",
+  	"Pho. Ha. Good god ya'll. What it isnt good for? Absolutely nothing.",
+  	"Frankly, my dear, i don't give a pho.",
+  	"NO I CAN'T STOP EATING PHO! THIS IS HOW I EAT! HAVENT YOU SEEN MY MOVIES?!"
+  	].sample
+  haml :index, locals: {catch_phrase: catch_phrase}
 end
 
 get '/success' do
